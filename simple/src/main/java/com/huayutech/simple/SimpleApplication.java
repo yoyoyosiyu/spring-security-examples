f706919bf28d6ebad15e4bb3a3b667c232a42aed
yoyoyosiyu <yoyoyosiyu@126.com>
2018-06-19 17:41:42 +0800

    custom 项目有些更改

--- simple/src/main/java/com/huayutech/simple/SimpleApplication.java
+++ simple/src/main/java/com/huayutech/simple/SimpleApplication.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -29,7 +30,9 @@ public class SimpleApplication extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin();
 
-        http.authorizeRequests().antMatchers("/userinfo").authenticated();
+        http.authorizeRequests().antMatchers("/userinfo").hasAnyRole("USER", "ADMIN");
+        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+        //http.authorizeRequests().anyRequest().authenticated().withObjectPostProcessor()
 
         // 在开启csrf的情况下使用，如果关闭csrf的情况下可以注释掉
         http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"));
@@ -45,7 +48,11 @@ public class SimpleApplication extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("user")).roles("USER");
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("user")).roles("USER")
+            .and()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
     }
 
     @Override
@@ -54,6 +61,11 @@ public class SimpleApplication extends WebSecurityConfigurerAdapter {
 
     @GetMapping("/userinfo")
     protected Principal doUserInfo(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/admin/userinfo")
+    protected Principal doGetAdminUserInfo(Principal principal) {
         return principal;
     }
 }
